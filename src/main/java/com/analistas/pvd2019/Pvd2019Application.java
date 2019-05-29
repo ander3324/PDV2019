c1d8e1b607a96a58d81a81f19d7cb5395a48d6cc
Bruno Conte <elbrun22@gmail.com>
2019-05-28 21:51:46 -0300

    creacion de proyecto y startUML 2

---
+++ src/main/java/com/analistas/pvd2019/Pvd2019Application.java
@@ -0,0 +1,13 @@
+package com.analistas.pvd2019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pvd2019Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pvd2019Application.class, args);
+	}
+
+}
