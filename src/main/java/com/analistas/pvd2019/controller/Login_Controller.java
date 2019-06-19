db337bbed6c45195c04ea613e5121422ad7d830d
nahuel <nahuel_97kpo@live.com.ar>
2019-06-19 18:25:02 -0300

    Clases

---
+++ src/main/java/com/analistas/pvd2019/controller/Login_Controller.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.analista.Prueba_punto_venta.controller;
+
+/**
+ *
+ * @author nahuel
+ */
+public class Login_Controller {
+    
+}
--- src/test/java/com/analistas/pvd2019/Pvd2019ApplicationTests.java
+++ src/test/java/com/analistas/pvd2019/Pvd2019ApplicationTests.java
@@ -11,6 +11,7 @@ public class Pvd2019ApplicationTests {
 
 	@Test
 	public void contextLoads() {
+            
 	}
 
 }
