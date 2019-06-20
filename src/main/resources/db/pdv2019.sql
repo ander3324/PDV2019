49698bbc76889c0627afd6d68255ce6ad9aa3741
MESistemas <matiasespindolarp@hotmail.com>
2019-06-20 19:59:12 -0300

    Modificado Base de Datos y Adoo

--- src/main/resources/db/pdv2019.sql
+++ src/main/resources/db/pdv2019.sql
@@ -1,21 +1,23 @@
+-- 20/06/2019 --
+
 DROP DATABASE pdv2019;
 CREATE DATABASE pdv2019;
 USE pdv2019;
 
-CREATE TABLE Rol(
+CREATE TABLE Roles(
      pk_id_rol int not null primary key auto_increment,
      rol varchar(25) not null,
      autorithy varchar(25) not null
 );
 
-CREATE TABLE Usuario(
+CREATE TABLE Usuarios(
     pk_id_us int not null primary key auto_increment,
-    user varchar(25) not null,
-    pass varchar(25) not null,
+    us varchar(25) not null,
+    ps varchar(25) not null,
     hab varchar(25) not null
 ); 
 
-CREATE TABLE Producto(
+CREATE TABLE Productos(
 	pk_id_prod int not null primary key auto_increment,
     nombre varchar(25) not null,
     descu varchar(25) not null,
@@ -24,14 +26,14 @@ CREATE TABLE Producto(
     cantidad int not null
 );
 
-CREATE TABLE Compra(
+CREATE TABLE Compras(
 	pk_id_com int not null primary key auto_increment,
     descu double not null,
 	monto double not null,
     cantidad int not null
 );
 
-CREATE TABLE Detalles_Compra(
+CREATE TABLE Detalles_Compras(
 	pk_id_dc int not null primary key auto_increment,
     fecha date not null,
     hora datetime not null,
@@ -39,12 +41,14 @@ CREATE TABLE Detalles_Compra(
     estado varchar(25) not null 
 );
 
+/*
 CREATE TABLE HistorialCompra(
     pk_id_hc int not null primary key auto_increment,
     fecha date not null
 );
+*/
 
-CREATE TABLE Proveedor(
+CREATE TABLE Proveedores(
 	pk_id_prov int not null primary key auto_increment,
     nombre varchar(25) not null,
 	direccion varchar(25) not null,
@@ -53,25 +57,25 @@ CREATE TABLE Proveedor(
     email varchar(25) not null
 );
 
-CREATE TABLE Ciudad(
+CREATE TABLE Ciudades(
 	pk_id_ciu int not null primary key auto_increment,
     ciudad varchar(25) not null,
     cp varchar(25) not null
 );
 
-CREATE TABLE Provincia(
+CREATE TABLE Provincias(
 	pk_id_prov int not null primary key auto_increment,
     provincia varchar(25) not null
 );
 
-CREATE TABLE Venta(
+CREATE TABLE Ventas(
 	pk_id_ven int not null primary key auto_increment,
     descu double not null,
 	monto double not null,
     cantidad int not null
 );
 
-CREATE TABLE Detalles_Venta(
+CREATE TABLE Detalles_Ventas(
 	pk_id_dv int not null primary key auto_increment,
     fecha date not null,
     hora datetime not null,
@@ -79,12 +83,14 @@ CREATE TABLE Detalles_Venta(
     estado varchar(25) not null 
 );
 
+/*
 CREATE TABLE HistorialVenta(
     pk_id_hv int not null primary key auto_increment,
     fecha date not null
 );
+*/
 
-CREATE TABLE Cliente(
+CREATE TABLE Clientes(
 	pk_id_cli int not null primary key auto_increment,
 	nombre varchar(25) not null,
     apellido varchar(25) not null,
@@ -96,35 +102,59 @@ CREATE TABLE Cliente(
     obs varchar(25) not null
 );
 
--- Agregar Columnas
+-- Agregar Columnas Modificado MatiasEspindola 20/06/2019 --
+ALTER TABLE Ciudades ADD COLUMN fk_id_provincia INT NULL;
+ALTER TABLE Proveedores ADD COLUMN fk_id_ciu INT NULL;
+ALTER TABLE Productos ADD COLUMN fk_id_prov INT NULL;
+ALTER TABLE Detalles_Compras ADD COLUMN fk_id_comp INT NULL;
+ALTER TABLE Compras ADD COLUMN fk_id_prod INT NULL;
+ALTER TABLE Ventas ADD COLUMN fk_id_producto INT NULL;
+ALTER TABLE Ventas ADD COLUMN fk_id_us INT NULL;
+ALTER TABLE Compras ADD COLUMN fk_id_usuario INT NULL;
+ALTER TABLE Detalles_Ventas ADD COLUMN fk_id_cli INT NULL;
+ALTER TABLE Detalles_Ventas ADD COLUMN fk_id_vent INT NULL;
+ALTER TABLE Clientes ADD COLUMN fk_id_ciudad INT NULL;
 
-ALTER TABLE Producto ADD COLUMN fk_id_prov INT NULL;
-ALTER TABLE Detalles_Venta ADD COLUMN fk_id_cli INT NULL;
-ALTER TABLE Detalles_Venta ADD COLUMN fk_id_vent INT NULL;
-ALTER TABLE Detalles_Compra ADD COLUMN fk_id_prov INT NULL;
-ALTER TABLE Detalles_Compra ADD COLUMN fk_id_comp INT NULL;
-ALTER TABLE Ciudad ADD COLUMN fk_id_provincia INT NULL;
-ALTER TABLE Proveedor ADD COLUMN fk_id_ciu INT NULL;
+-- MatiasEspindola 20/06/2019 --
+
+ALTER TABLE Usuarios ADD COLUMN fk_id_rol INT NULL;
 
 -- Agregar Relaciones
 
-ALTER TABLE Producto ADD CONSTRAINT fk_id_prov
-FOREIGN KEY (fk_id_prov) REFERENCES Proveedor(pk_id_prov);
+ALTER TABLE Productos ADD CONSTRAINT fk_id_prov
+FOREIGN KEY (fk_id_prov) REFERENCES Proveedores(pk_id_prov);
 
-ALTER TABLE Detalles_Venta ADD CONSTRAINT fk_id_cli
-FOREIGN KEY (fk_id_cli) REFERENCES Cliente(pk_id_cli);
+ALTER TABLE Detalles_Ventas ADD CONSTRAINT fk_id_cli
+FOREIGN KEY (fk_id_cli) REFERENCES Clientes(pk_id_cli);
 
-ALTER TABLE Detalles_Venta ADD CONSTRAINT fk_id_vent
-FOREIGN KEY (fk_id_vent) REFERENCES Venta(pk_id_ven);
+ALTER TABLE Detalles_Ventas ADD CONSTRAINT fk_id_vent
+FOREIGN KEY (fk_id_vent) REFERENCES Ventas(pk_id_ven);
 
-ALTER TABLE Detalles_Compra ADD CONSTRAINT fk_id_cli
-FOREIGN KEY (fk_id_cli) REFERENCES Cliente(pk_id_cli);
+ALTER TABLE Detalles_Compras ADD CONSTRAINT fk_id_comp
+FOREIGN KEY (fk_id_comp) REFERENCES Compras(pk_id_com);
 
-ALTER TABLE Detalles_Compra ADD CONSTRAINT fk_id_comp
-FOREIGN KEY (fk_id_comp) REFERENCES Compra(pk_id_com);
+ALTER TABLE Ciudades ADD CONSTRAINT fk_id_provincia
+FOREIGN KEY (fk_id_provincia) REFERENCES Provincias(pk_id_prov);
 
-ALTER TABLE Ciudad ADD CONSTRAINT fk_id_provincia
-FOREIGN KEY (fk_id_provincia) REFERENCES Provincia(pk_id_prov);
+ALTER TABLE Proveedores ADD CONSTRAINT fk_id_ciu
+FOREIGN KEY (fk_id_ciu) REFERENCES Ciudades(pk_id_ciu);
 
-ALTER TABLE Proveedor ADD CONSTRAINT fk_id_ciu
-FOREIGN KEY (fk_id_ciu) REFERENCES Ciudad(pk_id_ciu);
+-- Agregado MatiasEspindola 20/06/2019
+
+ALTER TABLE Clientes ADD CONSTRAINT fk_id_ciudad
+FOREIGN KEY (fk_id_ciudad) REFERENCES Ciudades(pk_id_ciu);
+
+ALTER TABLE Compras ADD CONSTRAINT fk_id_prod
+FOREIGN KEY (fk_id_prod) REFERENCES Productos(pk_id_prod);
+
+ALTER TABLE Ventas ADD CONSTRAINT fk_id_producto
+FOREIGN KEY (fk_id_producto) REFERENCES Productos(pk_id_prod);
+
+ALTER TABLE Ventas ADD CONSTRAINT fk_id_us
+FOREIGN KEY (fk_id_us) REFERENCES Usuarios(pk_id_us);
+
+ALTER TABLE Compras ADD CONSTRAINT fk_id_usuario
+FOREIGN KEY (fk_id_usuario) REFERENCES Usuarios(pk_id_us);
+
+ ALTER TABLE Usuarios ADD CONSTRAINT fk_id_rol
+FOREIGN KEY (fk_id_rol) REFERENCES Roles(pk_id_rol);
