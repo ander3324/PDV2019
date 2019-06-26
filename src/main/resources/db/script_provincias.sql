a84218354dcd54e005d0cb75eaa9cdacc2a5fefc
MESistemas <matiasespindolarp@hotmail.com>
2019-06-26 20:29:45 -0300

    Subido cambios

---
+++ src/main/resources/db/script_provincias.sql
@@ -0,0 +1,26 @@
+INSERT INTO provincias(pk_id_prov,provincia) VALUES (1,'Buenos Aires');
+INSERT INTO provincias(pk_id_prov,provincia) VALUES (2,'Buenos Aires-GBA');
+INSERT INTO provincias(pk_id_prov,provincia) VALUES (3,'Ciudad de Buenos Aires');
+INSERT INTO provincias(pk_id_prov,provincia) VALUES (4,'Catamarca');
+INSERT INTO provincias(pk_id_prov,provincia) VALUES (5,'Chaco');
+INSERT INTO provincias(pk_id_prov,provincia) VALUES (6,'Chubut');
+INSERT INTO provincias(pk_id_prov,provincia) VALUES (7,'Córdoba');
+INSERT INTO provincias(pk_id_prov,provincia) VALUES (8,'Corrientes');
+INSERT INTO provincias(pk_id_prov,provincia) VALUES (9,'Entre Ríos');
+INSERT INTO provincias(pk_id_prov,provincia) VALUES (10,'Formosa');
+INSERT INTO provincias(pk_id_prov,provincia) VALUES (11,'Jujuy');
+INSERT INTO provincias(pk_id_prov,provincia) VALUES (12,'La Pampa');
+INSERT INTO provincias(pk_id_prov,provincia) VALUES (13,'La Rioja');
+INSERT INTO provincias(pk_id_prov,provincia) VALUES (14,'Mendoza');
+INSERT INTO provincias(pk_id_prov,provincia) VALUES (15,'Misiones');
+INSERT INTO provincias(pk_id_prov,provincia) VALUES (16,'Neuquén');
+INSERT INTO provincias(pk_id_prov,provincia) VALUES (17,'Río Negro');
+INSERT INTO provincias(pk_id_prov,provincia) VALUES (18,'Salta');
+INSERT INTO provincias(pk_id_prov,provincia) VALUES (19,'San Juan');
+INSERT INTO provincias(pk_id_prov,provincia) VALUES (20,'San Luis');
+INSERT INTO provincias(pk_id_prov,provincia) VALUES (21,'Santa Cruz');
+INSERT INTO provincias(pk_id_prov,provincia) VALUES (22,'Santa Fe');
+INSERT INTO provincias(pk_id_prov,provincia) VALUES (23,'Santiago del Estero');
+INSERT INTO provincias(pk_id_prov,provincia) VALUES (24,'Tierra del Fuego');
+INSERT INTO provincias(pk_id_prov,provincia) VALUES (25,'Tucumán');
+
